f79b4c5ef02b5f8e141ce51fa2bfcef58358e9a9
Medsalemabdi <medsalemabd@gmail.com>
2025-12-31 14:44:20 +0100

    first commit setting up the environnement

---
+++ android/app/src/main/kotlin/com/occazcar/occazcar/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.occazcar.occazcar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'OccazCar',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text(
+            'OccazCar 🚗',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
+}
