9e6da3eae4a53cb54ac6814205159e0bb31cbd81
rami <rami.benamor@ensi-uma.tn>
2026-01-03 16:44:21 +0100

    feature photos

--- lib/features/ia/ai_service.dart
+++ lib/features/ia/ai_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 
 class AIService {
   // ⚠️ Hugging Face Read Token
-  static const String _apiKey = '*****';
+  static const String _apiKey = 'xxx';
 
   static const String _endpoint =
       'https://router.huggingface.co/v1/chat/completions';
